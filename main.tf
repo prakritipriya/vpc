c320890dcded122134faa181ba5bb879c7ffe65c
prakritipriya <112262065+prakritipriya@users.noreply.github.com>
2024-02-06 07:15:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "dev_ec2" {
   instance_type = "t3.micro"
 depends_on     = [aws_vpc.main]
 
-  security_groups = aws_security_group.allow_all.id
+  
   tags = {
     Name = "HelloWorld"
   }

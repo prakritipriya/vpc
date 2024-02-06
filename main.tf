b0331cdc15655433ad94b80f33c563e3ababd9cb
prakritipriya <112262065+prakritipriya@users.noreply.github.com>
2024-02-06 08:00:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "dev_ec2" {
   instance_type = "t3.micro"
 depends_on     = [aws_vpc.main]
 
- subnet_id =[aws_subnet.public_subsets]
+ subnet_id =[aws_subnet.public_subsets.id]
  vpc_security_group_ids = aws_security_group.allow_all.id 
   tags = {
     Name = "HelloWorld"

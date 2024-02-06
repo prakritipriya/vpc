09e407ed8ef41530a2eb5f0457e5b4f8b3eef46a
prakritipriya <112262065+prakritipriya@users.noreply.github.com>
2024-02-06 08:32:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_instance" "dev_ec2" {
   instance_type = "t2.micro"
 depends_on     = [aws_vpc.main]
 
- subnet_id =[aws_subnet.public_subsets[count.index].id]
+ subnet_id =aws_subnet.public_subsets[count.index].id
 # vpc_security_group_ids = aws_security_group.allow_all.id 
 count=length(var.public_subnet_cidrs)
 vpc_security_group_ids      = [aws_security_group.allow_all.id]
